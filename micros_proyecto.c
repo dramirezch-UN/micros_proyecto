96d1fe32eb620f7efb6fb74aa25af5806640a4c2
Daniel <dramirezch@unal.edu.co>
2022-06-25 18:14:09 -0500

    cada 100ms se suma uno a una variable que se muestra en el LCD (el LCD se actualiza cada 100ms)

--- micros_proyecto.c
+++ micros_proyecto.c
@@ -3,14 +3,53 @@
 #include "LibLCDXC8.h"
 #pragma config FOSC=INTOSC_EC
 #pragma config WDT=OFF
+
+
+char _B; //para poder borrar la bandera RBIF
+int b5Counter = 0;
+
+
+void interrupt ISR(void);
+
+
 void main(void){
+    // //Para la interrupcion de teclado
+    // TRISB=0b00100000;
+    // LATB=0b00000000;
+    // _B=PORTB; //para poder borrar la bandera RBIF
+    // RBPU=0; //resistencia pull up para las entradas
+    // __delay_ms(100);
+    // RBIF=0;
+    // RBIE=1;
+
+    // GIE=1;
+
+    //Para el LCD    
     TRISD=0;
     LATD=0;
     __delay_ms(1000);
     ConfiguraLCD(4);
     InicializaLCD();
-    //MensajeLCD_Var("Hola mundo");
-    MensajeLCD_Var("Hola mundo");
+    EscondeCursorLCD();
+
+    MensajeLCD_Var("Hola ");
+    __delay_ms(1000);
+    BorraLCD();
+
     while(1){
+        EscribeLCD_n16(b5Counter, 5);
+        __delay_ms(100);
+        BorraLCD();
     }    
 }
+
+
+// void interrupt ISR(void){
+//     if (RBIF==1){
+//         _B=PORTB; //para poder borrar la bandera RBIF
+//         RBIF=0;
+//         if ((_B&0b00100000)==0b00100000){
+//             b5Counter++;
+//         }
+//     }
+// }
