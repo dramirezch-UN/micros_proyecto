c0d244586c5e9e3c9c97455aca7fc20ce9bff45e
Daniel <dramirezch@unal.edu.co>
2022-06-25 20:38:04 -0500

    ahora cuenta al presionar un boton conectado a RB5

--- micros_proyecto.c
+++ micros_proyecto.c
@@ -4,6 +4,8 @@
 #pragma config FOSC=INTOSC_EC
 #pragma config WDT=OFF
 
+#pragma config LVP=OFF // libera rb5
+
 
 char _B; //para poder borrar la bandera RBIF
 int b5Counter = 0;
@@ -13,16 +15,25 @@ void interrupt ISR(void);
 
 
 void main(void){
-    // //Para la interrupcion de teclado
+    //Para la interrupcion de teclado
     // TRISB=0b00100000;
     // LATB=0b00000000;
-    // _B=PORTB; //para poder borrar la bandera RBIF
     // RBPU=0; //resistencia pull up para las entradas
     // __delay_ms(100);
+    // _B=PORTB; //para poder borrar la bandera RBIF
     // RBIF=0;
     // RBIE=1;
+    // PEIE=1;
 
-    // GIE=1;
+    //configura timer 0. 
+    T0CON=0b00000001; // 16 bits, temporizador, prescaler 4
+    TMR0=59286;
+    TMR0IF=0;
+    TMR0IE=1;
+    GIE=1;
+
+    TRISB=0b00100000;
+    LATB=0;
 
     //Para el LCD    
     TRISD=0;
@@ -36,15 +47,22 @@ void main(void){
     __delay_ms(1000);
     BorraLCD();
 
+    TMR0ON=1; //iniciar timer 0
     while(1){
-        EscribeLCD_n16(b5Counter, 5);
-        __delay_ms(100);
-        BorraLCD();
+        if ((PORTB&0b00100000)==0b00100000){
+            b5Counter++;
+        }
     }    
 }
 
 
-// void interrupt ISR(void){
+void interrupt ISR(void){
+    if(TMR0IF==1){
+        BorraLCD();
+        EscribeLCD_n16(b5Counter, 5);
+        TMR0=59286;
+        TMR0IF=0;
+    }
 //     if (RBIF==1){
 //         _B=PORTB; //para poder borrar la bandera RBIF
 //         RBIF=0;
@@ -52,4 +70,4 @@ void main(void){
 //             b5Counter++;
 //         }
 //     }
-// }
+}
